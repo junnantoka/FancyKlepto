5acbe9722115a5d794df5d271f7bfeed5b797dd0
Jun <junnantoka@gmail.com>
2020-03-08 15:58:01 +0100

    Add Guard class

---
+++ GameObjects/Guard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using FancyKlepto.GameManagement;
+
+namespace FancyKlepto.GameObjects
+{
+    class Guard : GameObject
+    {
+        public Guard() : base("spr_circle")
+        {
+            Reset();
+            position = new Vector2(400, 100);
+            velocity = new Vector2(40, 40);
+        }
+
+
+    }
+}
