1fbffd8258568285d807cf43c0a74de0ab5c3cac
BelloIsMe <Olgergames17@gmail.com>
2020-03-11 11:16:41 +0100

    Uitzetten van de lazers voor test

--- GameStates/PlayingState.cs
+++ GameStates/PlayingState.cs
@@ -20,11 +20,11 @@ namespace FancyKlepto.GameStates
             gameObjectList.Add(new Map("Level_A"));
             gameObjectList.Add(player);
             //StartPositie, SpriteName,LengteEnBreedte,Angle
-            gameObjectList.Add(new Laser(new Vector2(100, 100), "spr_laser_pixel_green", new Vector2(100, 5), (float)Math.PI / 2));
+            /*gameObjectList.Add(new Laser(new Vector2(100, 100), "spr_laser_pixel_green", new Vector2(100, 5), (float)Math.PI / 2));
             gameObjectList.Add(new Laser(new Vector2(100, 100), "spr_laser_pixel_yellow", new Vector2(100, 5), (float)Math.PI));
             gameObjectList.Add(new Laser(new Vector2(100, 100), "spr_laser_pixel", new Vector2(100, 5), (float)Math.PI * 1.5f));
             gameObjectList.Add(new Laser(new Vector2(100, 100), "spr_laser_pixel_purple", new Vector2(100, 5), (float)Math.PI * 2f));
-            gameObjectList.Add(new Laser(new Vector2(100, 100), "spr_laser_pixel_yellow", new Vector2(100, 5), (float)Math.PI / 4f));
+            gameObjectList.Add(new Laser(new Vector2(100, 100), "spr_laser_pixel_yellow", new Vector2(100, 5), (float)Math.PI / 4f)); */
             gameObjectList.Add(new Guard());
         }
 
