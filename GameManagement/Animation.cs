eb64e2e94de5c7bb9cf5c91f597b9541909cbeed
Am0rA <mr.ibrahim.topchu@outlook.com>
2020-04-10 21:42:33 +0200

    animation code fault
    verwijderd

--- GameManagement/Animation.cs
+++ GameManagement/Animation.cs
@@ -56,22 +56,5 @@ public class Animation : SpriteSheet
     {
         get { return !isLooping && sheetIndex >= NumberSheetElements - 1; }
     }
-    public int CurrentFrame { get; set; }
-    public int FrameCount { get; private set; }
-    public int FrameHeight { get; { return Texture.Height; } }
-    public int FrameSpeed { get; set; }
-    public int FrameWidth { get; { return Texture.Width / FrameCount; } }
-    public bool Islooping { get; set; }
-    public Texture2D Texture { get; private set; }
-    public Animation (Texture2D texture, int frameCount)
-    {
-        Texture = texture;
-
-        FrameCount = frameCount;
-
-        IsLooping = true;
-
-        FrameSpeed = 0.2f;
-    }
 }
 
