a9a008c8b0cd54a2883314432aa3a492c3cbd8fd
Jun <junnantoka@gmail.com>
2020-05-07 17:04:30 +0200

    Update startstate sprite

--- GameStates/StartState.cs
+++ GameStates/StartState.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
+using MonoSprites;
 
 namespace FancyKlepto.GameStates
 {
     class StartState : GameObjectList
     {
+        SpriteGameObject background;
         TextGameObject start;
         int timer;
         int seconds;
         public StartState()
         {
+            background = new SpriteGameObject("TitleScreen");
             start = new TextGameObject("Score");
+            this.Add(background);
             this.Add(start);
 
-            start.position = new Vector2(GameEnvironment.Screen.X / 2, GameEnvironment.Screen.Y / 2);
+            background.Position = GameEnvironment.Screen.ToVector2() / 2;
+            background.Origin = new Vector2(background.Width / 2, background.Height / 2);
+
+            start.position = GameEnvironment.Screen.ToVector2() / 2;
             start.text = "Press ENTER to start";
             start.Open = true;
             timer = 0;
@@ -36,7 +43,7 @@ namespace FancyKlepto.GameStates
             if (seconds > 2)
             {
                 start.Open = false;
-                if(seconds > 4)
+                if (seconds > 4)
                 {
                     start.Open = true;
                     timer = 0;
