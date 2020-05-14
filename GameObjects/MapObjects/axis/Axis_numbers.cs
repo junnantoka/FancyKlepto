9e2ef010bdb4110237c1a5bdb243eb44662b66ba
Jun <junnantoka@gmail.com>
2020-05-14 14:27:12 +0200

    Update Axis_nums with shadow

--- GameObjects/MapObjects/axis/Axis_numbers.cs
+++ GameObjects/MapObjects/axis/Axis_numbers.cs
@@ -12,12 +12,22 @@ namespace FancyKlepto.GameObjects.MapObjects.axis
         public Axis_numbers(int x, int y)
         {
             Reset();
+            const int OFFSET = 1;
 
             for (int i = 0; i < 30; i++)
             {
                 TextGameObject xaxis_Numbers = new TextGameObject("Score");
                 xaxis_Numbers.text = (i - x).ToString();
                 xaxis_Numbers.position = new Vector2(i * (unitSize + unitSpacing), y * (unitSize + unitSpacing));
+                xaxis_Numbers.Color = Color.Black;
+                Add(xaxis_Numbers);
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                TextGameObject xaxis_Numbers = new TextGameObject("Score");
+                xaxis_Numbers.text = (i - x).ToString();
+                xaxis_Numbers.position = new Vector2(i * (unitSize + unitSpacing) - OFFSET, y * (unitSize + unitSpacing) - OFFSET);
+                xaxis_Numbers.Color = new Color(0, 255, 0);
                 Add(xaxis_Numbers);
             }
             for (int i = 0; i < 17; i++)
@@ -25,6 +35,15 @@ namespace FancyKlepto.GameObjects.MapObjects.axis
                 TextGameObject yaxis_Numbers = new TextGameObject("Score");
                 yaxis_Numbers.text = (i - y).ToString();
                 yaxis_Numbers.position = new Vector2(x * (unitSize + unitSpacing), i * (unitSize + unitSpacing));
+                yaxis_Numbers.Color = Color.Black;
+                Add(yaxis_Numbers);
+            }
+            for (int i = 0; i < 17; i++)
+            {
+                TextGameObject yaxis_Numbers = new TextGameObject("Score");
+                yaxis_Numbers.text = (i - y).ToString();
+                yaxis_Numbers.position = new Vector2(x * (unitSize + unitSpacing) - OFFSET, i * (unitSize + unitSpacing) - OFFSET);
+                yaxis_Numbers.Color = new Color(255, 255, 0);
                 Add(yaxis_Numbers);
             }
         }
