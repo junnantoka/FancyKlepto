7c6af6b497dd1d05427e73378208461e9ab33de3
Jun <junnantoka@gmail.com>
2020-05-28 10:30:53 +0200

    Added 2 guards to Level3

--- GameStates/Level3.cs
+++ GameStates/Level3.cs
@@ -123,6 +123,9 @@ namespace FancyKlepto.GameStates
             switchBoards.Add(new SwitchBoard(14, 3, Color.Blue));
             switchBoards.Add(new SwitchBoard(4, 3, Color.Yellow));
             switchBoards.Add(new SwitchBoard(19, 3, Color.DarkGray));
+
+            guards.Add(new Guard(new Vector2(2, 9), new Vector2(25, 6)));
+            guards.Add(new Guard(new Vector2(25, 6), new Vector2(3, 8)));
         }
         public override void Reset()
         {
